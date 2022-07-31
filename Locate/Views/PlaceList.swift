37ad143f15419580e56c875f63bc45455f29f620
Jason Barrie Morley <jbmorley@mac.com>
2022-07-31 10:13:20 +0100

    Colored tags

--- Locate/Views/PlaceList.swift
+++ Locate/Views/PlaceList.swift
@@ -3,15 +3,18 @@ import SwiftUI
 struct TagList: View {
 
     var items: [String]
+    var color: Color
 
     var body: some View {
         HStack {
             ForEach(items) { item in
                 Text(item)
+                    .lineLimit(1)
                     .font(.footnote)
+                    .foregroundColor(color)
                     .padding([.top, .bottom], 4)
                     .padding([.leading, .trailing], 8)
-                    .background(Color.gray.opacity(0.3))
+                    .background(color.opacity(0.3))
                     .clipShape(Capsule())
             }
         }
@@ -34,7 +37,7 @@ struct PlaceList: View {
                 VStack(alignment: .leading) {
                     Text(place.address)
                     if let tags = place.tags, !tags.isEmpty {
-                        TagList(items: tags)
+                        TagList(items: tags, color: .purple)
                     }
                 }
                 .lineLimit(1)
