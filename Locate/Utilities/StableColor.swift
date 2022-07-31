6c5056e4b76fa6bae6f6265fe9e2fe0d6826d5d2
Jason Barrie Morley <jbmorley@mac.com>
2022-07-31 21:22:08 +0100

    Colored tags

--- Locate/Utilities/StableColor.swift
+++ Locate/Utilities/StableColor.swift
@@ -1,9 +1,28 @@
 import SwiftUI
 
+import HashRainbow
+
+extension Array where Element == Color {
+
+    static let system: [Color] = [
+        .pink,
+        .purple,
+        .orange,
+        .green,
+        .mint,
+        .yellow,
+        .teal,
+        .red,
+        .indigo,
+        .cyan,
+    ]
+
+}
+
 extension String {
 
     func color() -> Color {
-        return .teal    
+        return HashRainbow.colorForString(self, colors: .system)
     }
 
 }
