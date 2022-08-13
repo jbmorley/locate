6b8948975d4cc814477d15da192392763d97760b
Jason Barrie Morley <jbmorley@mac.com>
2022-08-13 05:01:58 +0100

    refactor: Catch up to Diligence with full macOS about screen support

--- Locate/LocateApp.swift
+++ Locate/LocateApp.swift
@@ -12,6 +12,7 @@ struct LocateApp: App {
     }
 
     var body: some Scene {
+
         Window("Locate", id: "main") {
             ContentView()
                 .environmentObject(model)
@@ -24,28 +25,23 @@ struct LocateApp: App {
             SelectionCommands(selection: model.selection)
             AboutCommands()
         }
-        About(repository: "jbmorley/locate") {
 
+        AboutWindowGroup(repository: "jbmorley/locate") {
             Action("GitHub", url: URL(string: "https://github.com/jbmorley/locate")!)
-
         } acknowledgements: {
-
             Acknowledgements("Developers") {
                 Credit("Jason Morley", url: URL(string: "https://jbmorley.co.uk"))
             }
-
             Acknowledgements("Thanks") {
                 Credit("Michael Dales")
                 Credit("Sarah Barbour")
             }
-
         } licenses: {
-
             License("Diligence", author: "InSeven Limited", filename: "diligence-license")
             License("HashRainbow", author: "Sarah Barbour", filename: "hash-rainbow-license")
             License("Interact", author: "InSeven Limited", filename: "interact-license")
             License("SwiftSoup", author: "Nabil Chatbi", filename: "swift-soup-license")
-
         }
+
     }
 }
